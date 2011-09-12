c617a98f319373d42d28e1adee6d432397eba7e1
tcroft@tscserver <trevor@croftfamily.net>
2011-09-12 14:14:06 -0500

    hide titlebar by default

--- src/com/coredroid/ui/CoreFragmentActivity.java
+++ src/com/coredroid/ui/CoreFragmentActivity.java
@@ -4,14 +4,22 @@ import com.coredroid.core.CoreApplication;
 import com.coredroid.util.UIUtil;
 
 import android.graphics.Typeface;
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 public class CoreFragmentActivity extends FragmentActivity {
 
 	private boolean initFinished;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+	}
 	
 	@Override
 	public void onContentChanged() {
